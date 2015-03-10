640e51a9540608a9108afb68ed263214b4b9f278
Konstantin Kitmanov <hogart@coccoc.vn>
2015-03-10 23:35:07 +0700

    grunt tasks for styles and templates

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function (grunt) {
+	'use strict';
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		stylus: {
+			options: {
+				'include css': true,
+				'resolve url': true
+			},
+			dev: {
+				options: {
+					compress: false,
+					sourcemap: {
+						inline: true
+					}
+				},
+				files: {
+					'public/index.css': 'styles/index.styl'
+				}
+			},
+			prod: {
+				options: {
+					compress: true
+				},
+				files: {
+					'public/index.min.css': 'styles/index.styl'
+				}
+			}
+		},
+
+		jade: {
+			serverDev: {
+				options: {
+					client: false,
+					pretty: true
+				},
+				files: {
+					'public/index.html': ['templates/server/index.jade']
+				}
+			},
+			serverProd: {
+				options: {
+					client: false,
+					pretty: false
+				},
+				files: {
+					'public/index.html': ['templates/server/index.jade']
+				}
+			},
+			clientDev: {
+				options: {
+					client: true,
+					amd: false,
+					compileDebug: true
+				},
+				files: {
+					'public/templates.js': ['templates/client/**/*.jade']
+				}
+			},
+			clientProd: {
+				options: {
+					client: true,
+					amd: false,
+					compileDebug: false
+				},
+				files: {
+					'public/templates.min.js': ['templates/client/**/*.jade']
+				}
+			}
+		},
+
+		watch: {
+			styles: {
+                files: ['styles/blocks/*/*.styl', 'styles/index.styl'],
+                tasks: ['stylus:dev']
+            },
+			templatesServer: {
+				files: ['templates/server/*.jade', 'templates/server/**/*.jade', 'templates/common/**/*.jade'],
+				tasks: ['jade:serverDev']
+			},
+			templatesClient: {
+				files: ['templates/client/*.jade', 'templates/client/**/*.jade', 'templates/common/**/*.jade'],
+				tasks: ['jade:clientDev']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+
+	grunt.registerTask('build', ['stylus:dev', 'jade:serverDev', 'jade:clientDev']);
+	grunt.registerTask('prod', ['stylus:prod', 'jade:serverProd', 'jade:clientProd']);
+	grunt.registerTask('default', ['build', 'watch']);
+};
