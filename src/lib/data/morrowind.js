307714bc3c61300194903ad7002a1c831f3626ee
Konstantin Kitmanov <hogart@coccoc.vn>
2015-04-01 01:45:29 +0700

    fix data inconsistencies

--- src/lib/data/morrowind.js
+++ src/lib/data/morrowind.js
@@ -1,360 +1,360 @@
-export let morrowind = [{
-	'title': 'Alit Hide',
+export default [{
+	name: 'Alit Hide',
 	icon: 'http://images.uesp.net/8/82/MW-icon-ingredient-Alit_Hide.png',
 	note: 'Found by killing Alits',
 	effects: ['Drain Intelligence', 'Resist Poison', 'Telekinesis', 'Detect Animal']
 }, {
-	'title': 'Ampoule Pod',
+	name: 'Ampoule Pod',
 	icon: 'http://images.uesp.net/2/20/MW-icon-ingredient-Ampoule_Pod.png',
 	note: 'Common growing in swamps in the Bitter Coast region; one of two possible outputs of the draggle-tail plant (the other is the Coda Flower)',
 	effects: ['Water Walking', 'Paralyze', 'Detect Animal', 'Drain Willpower']
 }, {
-	'title': 'Ash Salts',
+	name: 'Ash Salts',
 	icon: 'http://images.uesp.net/6/6e/MW-icon-ingredient-Ash_Salts.png',
 	note: 'Found by killing Ash creatures',
 	effects: ['Drain Agility', 'Resist Magicka', 'Cure Blight Disease', 'Resist Magicka']
 }, {
-	'title': 'Ash Yam',
+	name: 'Ash Yam',
 	icon: 'http://images.uesp.net/a/a0/MW-icon-ingredient-Ash_Yam.png',
 	note: 'Most common in farm areas, occasionally found in the wild',
 	effects: ['Fortify Intelligence', 'Fortify Strength', 'Resist Common Disease', 'Detect Key']
 }, {
-	'title': 'Bittergreen Petals',
+	name: 'Bittergreen Petals',
 	icon: 'http://images.uesp.net/e/e3/MW-icon-ingredient-Bittergreen_Petal.png',
 	note: 'Grows on thorny vines in drier regions',
 	effects: ['Restore Intelligence', 'Invisibility', 'Drain Endurance', 'Drain Magicka']
 }, {
-	'title': 'Black Anther',
+	name: 'Black Anther',
 	icon: 'http://images.uesp.net/8/82/MW-icon-ingredient-Black_Anther.png',
 	note: 'These flowers are most common in the Ascadian Isles region',
 	effects: ['Drain Agility', 'Resist Fire', 'Drain Endurance', 'Light']
 }, {
-	'title': 'Black Lichen',
+	name: 'Black Lichen',
 	icon: 'http://images.uesp.net/4/42/MW-icon-ingredient-Black_Lichen.png',
 	note: 'Somewhat rare, mostly found in caves',
 	effects: ['Drain Strength', 'Resist Frost', 'Drain Speed', 'Cure Poison']
 }, {
-	'title': 'Bloat',
+	name: 'Bloat',
 	icon: 'http://images.uesp.net/6/6e/MW-icon-ingredient-Ash_Salts.png',
 	note: 'Grows in Bloat Spores, which are unfortunately not found anywhere in the game. The only examples you can find are in random containers, or sold by vendors.',
 	effects: ['Drain Magicka', 'Fortify Intelligence', 'Fortify Willpower', 'Detect Animal']
 }, {
-	'title': 'Bonemeal',
+	name: 'Bonemeal',
 	icon: 'http://images.uesp.net/1/1a/MW-icon-ingredient-Bonemeal.png',
 	note: 'Found on skeletons (living or dead), and also in urns in most tombs.',
 	effects: ['Restore Agility', 'Telekinesis', 'Drain Fatigue', 'Drain Personality']
 }, {
-	'title': 'Bread',
+	name: 'Bread',
 	icon: 'http://images.uesp.net/a/a4/MW-icon-ingredient-Bread.png',
 	note: 'Found in most taverns and many houses.',
 	effects: ['Restore Fatigue']
 }, {
-	'title': 'Bungler\'s Bane',
+	name: 'Bungler\'s Bane',
 	icon: 'http://images.uesp.net/6/60/MW-icon-ingredient-Bungler%27s_Bane.png',
 	note: 'This shelf-fungus grows on tree trunks in the Bitter Coast region',
 	effects: ['Drain Speed', 'Drain Endurance', 'Dispel', 'Drain Strength']
 }, {
-	'title': 'Chokeweed',
+	name: 'Chokeweed',
 	icon: 'http://images.uesp.net/5/57/MW-icon-ingredient-Chokeweed.png',
 	note: 'Found growing in drier regions',
 	effects: ['Drain Luck', 'Restore Fatigue', 'Cure Common Disease', 'Drain Willpower']
 }, {
-	'title': 'Coda Flower',
+	name: 'Coda Flower',
 	icon: 'http://images.uesp.net/a/ac/MW-icon-ingredient-Coda_Flower.png',
 	note: 'Found growing in swampy pools in the Bitter Coast region on the draggle-tail plant',
 	effects: ['Drain Personality', 'Levitate', 'Drain Intelligence', 'Drain Health']
 }, {
-	'title': 'Comberry',
+	name: 'Comberry',
 	icon: 'http://images.uesp.net/5/50/MW-icon-ingredient-Comberry.png',
 	note: 'Grows in shrubs in the West Gash and Ascadian Isles regions',
 	effects: ['Drain Fatigue', 'Restore Magicka', 'Fire Shield', 'Reflect']
 }, {
-	'title': 'Corkbulb Root',
+	name: 'Corkbulb Root',
 	icon: 'http://images.uesp.net/6/64/MW-icon-ingredient-Corkbulb_Root.png',
 	note: 'Mostly found in drier regions in the wild.',
 	effects: ['Cure Paralyzation', 'Restore Health', 'Lightning Shield', 'Fortify Luck']
 }, {
-	'title': 'Corprus Weepings',
+	name: 'Corprus Weepings',
 	icon: 'http://images.uesp.net/9/92/MW-icon-ingredient-Corprus_Weepings.png',
 	note: 'Found by killing Corprus Stalkers',
 	effects: ['Drain Fatigue', 'Fortify Luck', 'Drain Willpower', 'Restore Health']
 }, {
-	'title': 'Crab Meat',
+	name: 'Crab Meat',
 	icon: 'http://images.uesp.net/3/37/MW-icon-ingredient-Crab_Meat.png',
 	note: 'Found by killing Mudcrabs',
 	effects: ['Restore Fatigue', 'Resist Shock', 'Lightning Shield', 'Restore Luck']
 }, {
-	'title': 'Daedra Skin',
+	name: 'Daedra Skin',
 	icon: 'http://images.uesp.net/1/12/MW-icon-ingredient-Daedra_Skin.png',
 	note: 'Somewhat rare, mostly found at vendors',
 	effects: ['Fortify Strength', 'Cure Common Disease', 'Paralyze', 'Swift Swim']
 }, {
-	'title': 'Daedra\'s Heart',
+	name: 'Daedra\'s Heart',
 	icon: 'http://images.uesp.net/7/7a/MW-icon-ingredient-Daedra%27s_Heart.png',
 	note: 'Found by killing most Daedra - Dremora, Ogrim, Clannfears, Daedroth, etc.',
 	effects: ['Restore Magicka', 'Fortify Endurance', 'Drain Agility', 'Night Eye']
 }, {
-	'title': 'Diamond',
+	name: 'Diamond',
 	icon: 'http://images.uesp.net/a/a5/MW-icon-ingredient-Diamond.png',
 	note: 'Common in barrels in Dwemer ruins. Also found in a small number of caves',
 	effects: ['Drain Agility', 'Invisibility', 'Reflect', 'Detect Key']
 }, {
-	'title': 'Dreugh Wax',
+	name: 'Dreugh Wax',
 	icon: 'http://images.uesp.net/c/c5/MW-icon-ingredient-Dreugh_Wax.png',
 	note: 'Found by killing Dreugh',
 	effects: ['Fortify Strength', 'Restore Strength', 'Drain Luck', 'Drain Willpower']
 }, {
-	'title': 'Ectoplasm',
+	name: 'Ectoplasm',
 	icon: 'http://images.uesp.net/7/77/MW-icon-ingredient-Ectoplasm.png',
 	note: 'Found by killing Ghosts',
 	effects: ['Fortify Agility', 'Detect Animal', 'Drain Strength', 'Drain Health']
 }, {
-	'title': 'Emerald',
+	name: 'Emerald',
 	icon: 'http://images.uesp.net/1/1d/MW-icon-ingredient-Emerald.png',
 	note: 'Common in barrels in Dwemer ruins',
 	effects: ['Fortify Magicka', 'Restore Health', 'Drain Agility', 'Drain Endurance']
 }, {
-	'title': 'Fire Petal',
+	name: 'Fire Petal',
 	icon: 'http://images.uesp.net/4/42/MW-icon-ingredient-Fire_Petal.png',
 	note: 'Found growing in the Molag Amur and Red Mountain regions',
 	effects: ['Resist Fire', 'Drain Health', 'Spell Absorption', 'Paralyze']
 }, {
-	'title': 'Fire Salts',
+	name: 'Fire Salts',
 	icon: 'http://images.uesp.net/a/a0/MW-icon-ingredient-Fire_Salts.png',
 	note: 'Found by killing Flame Atronachs',
 	effects: ['Drain Health', 'Fortify Agility', 'Resist Frost', 'Fire Shield']
 }, {
-	'title': 'Frost Salts',
+	name: 'Frost Salts',
 	icon: 'http://images.uesp.net/2/2b/MW-icon-ingredient-Frost_Salts.png',
 	note: 'Found by killing Frost Atronachs',
 	effects: ['Drain Speed', 'Restore Magicka', 'Frost Shield', 'Resist Fire']
 }, {
-	'title': 'Ghoul Heart',
+	name: 'Ghoul Heart',
 	icon: 'http://images.uesp.net/e/ea/MW-icon-ingredient-Ghoul_Heart.png',
 	note: 'Supposedly the heart tissue of an Ash Ghoul, but not found on them. Mostly in random ingredient containers and sold at vendors.',
 	effects: ['Paralyze', 'Cure Poison', 'Fortify Attack']
 }, {
-	'title': 'Gold Kanet',
+	name: 'Gold Kanet',
 	icon: 'http://images.uesp.net/6/6f/MW-icon-ingredient-Gold_Kanet.png',
 	note: 'Found growing in bushes in the grassy regions',
 	effects: ['Drain Health', 'Burden', 'Drain Luck', 'Restore Strength']
 }, {
-	'title': 'Gravedust',
+	name: 'Gravedust',
 	icon: 'http://images.uesp.net/a/a9/MW-icon-ingredient-Gravedust.png',
 	note: 'No reliable location. Occasionally for sale at vendors or found in random containers',
 	effects: ['Drain Intelligence', 'Cure Common Disease', 'Drain Magicka', 'Restore Endurance']
 }, {
-	'title': 'Green Lichen',
+	name: 'Green Lichen',
 	icon: 'http://images.uesp.net/e/e5/MW-icon-ingredient-Green_Lichen.png',
 	note: 'Mostly grows on rocks in caves',
 	effects: ['Fortify Personality', 'Cure Common Disease', 'Drain Strength', 'Drain Health']
 }, {
-	'title': 'Guar Hide',
+	name: 'Guar Hide',
 	icon: 'http://images.uesp.net/b/b4/MW-icon-ingredient-Guar_Hide.png',
 	note: 'Found by killing Guars',
 	effects: ['Drain Fatigue', 'Fortify Endurance', 'Restore Personality', 'Fortify Luck']
 }, {
-	'title': 'Hackle-Lo Leaf',
+	name: 'Hackle-Lo Leaf',
 	icon: 'http://images.uesp.net/d/d3/MW-icon-ingredient-Hackle-Lo_Leaf.png',
 	note: 'Found as food in most houses; grows wild in the Azura\'s Coast region.',
 	effects: ['Restore Fatigue', 'Paralyze', 'Water Breathing', 'Restore Luck']
 }, {
-	'title': 'Heather',
+	name: 'Heather',
 	icon: 'http://images.uesp.net/7/78/MW-icon-ingredient-Heather.png',
 	note: 'This common flower grows throughout the greener regions of Vvardenfell.',
 	effects: ['Restore Personality', 'Feather', 'Drain Speed', 'Drain Personality']
 }, {
-	'title': 'Hound Meat',
+	name: 'Hound Meat',
 	icon: 'http://images.uesp.net/b/b2/MW-icon-ingredient-Hound_Meat.png',
 	note: 'Found by killing Nix-Hounds',
 	effects: ['Restore Fatigue', 'Fortify Fatigue', 'Reflect', 'Detect Enchantment']
 }, {
-	'title': 'Hypha Facia',
+	name: 'Hypha Facia',
 	icon: 'http://images.uesp.net/e/e5/MW-icon-ingredient-Hypha_Facia.png',
 	note: 'This shelf-fungus is found growing on tree trunks in the Bitter Coast region',
 	effects: ['Drain Luck', 'Drain Agility', 'Drain Fatigue', 'Detect Enchantment']
 }, {
-	'title': 'Kagouti Hide',
+	name: 'Kagouti Hide',
 	icon: 'http://images.uesp.net/f/f7/MW-icon-ingredient-Kagouti_Hide.png',
 	note: 'Found by killing Kagoutis',
 	effects: ['Drain Fatigue', 'Fortify Speed', 'Resist Common Disease', 'Night Eye']
 }, {
-	'title': 'Kresh Fiber',
+	name: 'Kresh Fiber',
 	icon: 'http://images.uesp.net/c/c0/MW-icon-ingredient-Kresh_Fiber.png',
 	note: 'Kreshweed is found growing near sea water, most common in the Azura\'s Coast, Bitter Coast and West Gash regions.',
 	effects: ['Restore Luck', 'Fortify Personality', 'Drain Magicka', 'Drain Speed']
 }, {
-	'title': 'Kwama Cuttle',
+	name: 'Kwama Cuttle',
 	icon: 'http://images.uesp.net/b/b4/MW-icon-ingredient-Kwama_Cuttle.png',
 	note: 'Found by killing Kwama (any type).',
 	effects: ['Resist Poison', 'Drain Fatigue', 'Water Walking', 'Water Breathing']
 }, {
-	'title': 'Large Kwama Egg',
+	name: 'Large Kwama Egg',
 	icon: 'http://images.uesp.net/7/73/MW-icon-ingredient-Kwama_Egg.png',
 	note: 'Found in Egg Mines. Also common as food in houses.',
 	effects: ['Restore Fatigue', 'Paralyze', 'Frost Shield', 'Fortify Health']
 }, {
-	'title': 'Luminous Russula',
+	name: 'Luminous Russula',
 	icon: 'http://images.uesp.net/d/d3/MW-icon-ingredient-Luminous_Russula.png',
 	note: 'Found in the Bitter Coast region, also common in caves',
 	effects: ['Water Breathing', 'Drain Fatigue', 'Poison']
 }, {
-	'title': 'Marshmerrow',
+	name: 'Marshmerrow',
 	icon: 'http://images.uesp.net/f/fe/MW-icon-ingredient-Marshmerrow.png',
 	note: 'Found growing near water, most common in the Azura\'s Coast region',
 	effects: ['Restore Health', 'Detect Enchantment', 'Drain Willpower', 'Drain Fatigue']
 }, {
-	'title': 'Moon Sugar',
+	name: 'Moon Sugar',
 	icon: 'http://images.uesp.net/c/c2/MW-icon-ingredient-Moon_Sugar.png',
 	note: 'Found in smuggler caves. Most vendors will not deal with you if you are carrying this illegal narcotic substance.',
 	effects: ['Fortify Speed', 'Dispel', 'Drain Endurance', 'Drain Luck']
 }, {
-	'title': 'Muck',
+	name: 'Muck',
 	icon: 'http://images.uesp.net/2/23/MW-icon-ingredient-Muck.png',
 	note: 'Found on Muck Sponges which grow near water in many regions',
 	effects: ['Drain Intelligence', 'Detect Key', 'Drain Personality', 'Cure Common Disease']
 }, {
-	'title': 'Netch Leather',
+	name: 'Netch Leather',
 	icon: 'http://images.uesp.net/6/68/MW-icon-ingredient-Netch_Leather.png',
 	note: 'Found by killing Netch',
 	effects: ['Fortify Endurance', 'Fortify Intelligence', 'Drain Personality', 'Cure Paralyzation']
 }, {
-	'title': 'Pearl',
+	name: 'Pearl',
 	icon: 'http://images.uesp.net/c/cd/MW-icon-ingredient-Pearl.png',
 	note: 'Found in kollops on the sea floor. Also found in barrels in Dwemer ruins.',
 	effects: ['Drain Agility', 'Dispel', 'Water Breathing', 'Resist Common Disease']
 }, {
-	'title': 'Racer Plumes',
+	name: 'Racer Plumes',
 	icon: 'http://images.uesp.net/9/97/MW-icon-ingredient-Racer_Plumes.png',
 	note: 'Found by killing Cliff Racers',
 	effects: ['Drain Willpower', 'Levitate']
 }, {
-	'title': 'Rat Meat',
+	name: 'Rat Meat',
 	icon: 'http://images.uesp.net/3/38/MW-icon-ingredient-Rat_Meat.png',
 	note: 'Found by killing Rats',
 	effects: ['Drain Magicka', 'Paralyze', 'Cure Poison', 'Resist Poison']
 }, {
-	'title': 'Raw Ebony',
+	name: 'Raw Ebony',
 	icon: 'http://images.uesp.net/8/86/MW-icon-ingredient-Raw_Ebony.png',
 	note: 'Found in Ebony Mines. Usually a crime to take it.',
 	effects: ['Drain Agility', 'Cure Poison', 'Frost Shield', 'Restore Speed']
 }, {
-	'title': 'Raw Glass',
+	name: 'Raw Glass',
 	icon: 'http://images.uesp.net/b/b6/MW-icon-ingredient-Raw_Glass.png',
 	note: 'Found in Glass Mines. Usually a crime to take it.',
 	effects: ['Drain Intelligence', 'Drain Strength', 'Drain Speed', 'Fire Shield']
 }, {
-	'title': 'Red Lichen',
+	name: 'Red Lichen',
 	icon: 'http://images.uesp.net/2/22/MW-icon-ingredient-Red_Lichen.png',
 	note: 'Found growing on rocks, mostly in the Ashlands. Also for sale at vendors.',
 	effects: ['Drain Speed', 'Light', 'Cure Common Disease', 'Drain Magicka']
 }, {
-	'title': 'Resin',
+	name: 'Resin',
 	icon: 'http://images.uesp.net/4/4f/MW-icon-ingredient-Resin.png',
 	note: 'No reliable location. Occasionally for sale at vendors or found in random containers.',
 	effects: ['Restore Health', 'Restore Speed', 'Burden', 'Resist Common Disease']
 }, {
-	'title': 'Roobrush',
+	name: 'Roobrush',
 	icon: 'http://images.uesp.net/5/54/MW-icon-ingredient-Roobrush.png',
 	note: 'Found growing in hilly regions',
 	effects: ['Drain Willpower', 'Fortify Agility', 'Drain Health', 'Cure Poison']
 }, {
-	'title': 'Ruby',
+	name: 'Ruby',
 	icon: 'http://images.uesp.net/8/81/MW-icon-ingredient-Ruby.png',
 	note: 'Often found in barrels in Dwemer ruins',
 	effects: ['Drain Health', 'Feather', 'Restore Intelligence', 'Drain Agility']
 }, {
-	'title': 'Saltrice',
+	name: 'Saltrice',
 	icon: 'http://images.uesp.net/8/8b/MW-icon-ingredient-Saltrice.png',
 	note: 'Found growing near sea water, most common in the Azura\'s Coast region. Also a common food item in houses.',
 	effects: ['Restore Fatigue', 'Fortify Magicka', 'Drain Strength', 'Restore Health']
 }, {
-	'title': 'Scales',
+	name: 'Scales',
 	icon: 'http://images.uesp.net/f/fa/MW-icon-ingredient-Scales.png',
 	note: 'Found by killing Slaughterfish',
 	effects: ['Drain Personality', 'Water Walking', 'Restore Endurance', 'Swift Swim']
 }, {
-	'title': 'Scamp Skin',
+	name: 'Scamp Skin',
 	icon: 'http://images.uesp.net/e/eb/MW-icon-ingredient-Scamp_Skin.png',
 	note: 'Found by killing Scamps',
 	effects: ['Drain Magicka', 'Cure Paralyzation', 'Restore Personality', 'Restore Strength']
 }, {
-	'title': 'Scathecraw',
+	name: 'Scathecraw',
 	icon: 'http://images.uesp.net/d/db/MW-icon-ingredient-Scathecraw.png',
 	note: 'Grows in dry, hilly regions',
 	effects: ['Drain Strength', 'Cure Poison', 'Drain Health', 'Restore Willpower']
 }, {
-	'title': 'Scrap Metal',
+	name: 'Scrap Metal',
 	icon: 'http://images.uesp.net/7/73/MW-icon-ingredient-Scrap_Metal.png',
 	note: 'Found by killing Dwemer constructs. Also common in barrels and on shelves in Dwemer ruins.',
 	effects: ['Drain Health', 'Lightning Shield', 'Resist Shock', 'Restore Intelligence']
 }, {
-	'title': 'Scrib Jelly',
+	name: 'Scrib Jelly',
 	icon: 'http://images.uesp.net/7/75/MW-icon-ingredient-Scrib_Jelly.png',
 	note: 'Found by killing Scribs',
 	effects: ['Fortify Willpower', 'Cure Poison', 'Cure Blight Disease', 'Restore Willpower']
 }, {
-	'title': 'Scrib Jerky',
+	name: 'Scrib Jerky',
 	icon: 'http://images.uesp.net/4/41/MW-icon-ingredient-Scrib_Jerky.png',
 	note: 'Found as a food item in houses.',
 	effects: ['Restore Fatigue', 'Fortify Fatigue', 'Burden', 'Swift Swim']
 }, {
-	'title': 'Scuttle',
+	name: 'Scuttle',
 	icon: 'http://images.uesp.net/8/88/MW-icon-ingredient-Scuttle.png',
 	note: 'Found as a food item in houses.',
 	effects: ['Restore Fatigue', 'Fortify Fatigue', 'Feather', 'Telekinesis']
 }, {
-	'title': 'Shalk Resin',
+	name: 'Shalk Resin',
 	icon: 'http://images.uesp.net/e/e5/MW-icon-ingredient-Shalk_Resin.png',
 	note: 'Found by killing Shalks',
 	effects: ['Drain Fatigue', 'Fortify Health', 'Drain Personality', 'Fortify Speed']
 }, {
-	'title': 'Sload Soap',
+	name: 'Sload Soap',
 	icon: 'http://images.uesp.net/a/aa/MW-icon-ingredient-Sload_Soap.png',
 	note: 'Rare. Mostly found for sale at vendors or in random containers.',
 	effects: ['Drain Personality', 'Fortify Agility', 'Fire Shield', 'Restore Agility']
 }, {
-	'title': 'Small Kwama Egg',
+	name: 'Small Kwama Egg',
 	icon: 'http://images.uesp.net/7/73/MW-icon-ingredient-Kwama_Egg.png',
 	note: 'Found in Egg Mines. Also common food item in houses.',
 	effects: ['Restore Fatigue']
 }, {
-	'title': 'Spore Pod',
+	name: 'Spore Pod',
 	icon: 'http://images.uesp.net/a/a4/MW-icon-ingredient-Spore_Pod.png',
 	note: 'Found on Slough Ferns growing in the Bitter Coast region',
 	effects: ['Drain Strength', 'Drain Fatigue', 'Detect Key', 'Paralyze']
 }, {
-	'title': 'Stoneflower Petals',
+	name: 'Stoneflower Petals',
 	icon: 'http://images.uesp.net/1/17/MW-icon-ingredient-Stoneflower_Petals.png',
 	note: 'These flowers are common in the West Gash and Ascadian Isles regions.',
 	effects: ['Restore Strength', 'Fortify Magicka', 'Drain Luck', 'Fortify Personality']
 }, {
-	'title': 'Trama Root',
+	name: 'Trama Root',
 	icon: 'http://images.uesp.net/5/53/MW-icon-ingredient-Trama_Root.png',
 	note: 'Found growing in dry, hilly regions',
 	effects: ['Restore Willpower', 'Levitate', 'Drain Magicka', 'Drain Speed']
 }, {
-	'title': 'Vampire Dust',
+	name: 'Vampire Dust',
 	icon: 'http://images.uesp.net/8/80/MW-icon-ingredient-Vampire_Dust.png',
 	note: 'Found by killing Vampires',
 	effects: ['Fortify Health', 'Fortify Strength', 'Spell Absorption', 'Vampirism']
 }, {
-	'title': 'Violet Coprinus',
+	name: 'Violet Coprinus',
 	icon: 'http://images.uesp.net/8/8e/MW-icon-ingredient-Violet_Coprinus.png',
 	note: 'These mushrooms grow throughout the Bitter Coast region, and are also common in caves.',
 	effects: ['Water Walking', 'Drain Fatigue', 'Poison']
 }, {
-	'title': 'Void Salts',
+	name: 'Void Salts',
 	icon: 'http://images.uesp.net/0/00/MW-icon-ingredient-Void_Salts.png',
 	note: 'Found by killing Storm Atronachs or Winged Twilights',
 	effects: ['Restore Magicka', 'Spell Absorption', 'Paralyze', 'Drain Endurance']
 }, {
-	'title': 'Wickwheat',
+	name: 'Wickwheat',
 	icon: 'http://images.uesp.net/f/fc/MW-icon-ingredient-Wickwheat.png',
 	note: 'Found growing in the Grazelands region',
 	effects: ['Restore Health', 'Fortify Willpower', 'Paralyze', 'Damage Intelligence']
 }, {
-	'title': 'Willow Anther',
+	name: 'Willow Anther',
 	icon: 'http://images.uesp.net/c/c9/MW-icon-ingredient-Willow_Anther.png',
 	note: 'Most common in the Ascadian Isles region',
 	effects: ['Drain Personality', 'Frost Shield', 'Cure Common Disease', 'Cure Paralyzation']
