9a460963a689eb4d0ffcb24344c61ff1bffa6a72
Konstantin Kitmanov <hogart@coccoc.vn>
2015-04-01 12:53:39 +0700

    use generator instead of custom Iterable creation

--- src/Collection/Abstract.js
+++ src/Collection/Abstract.js
@@ -2,28 +2,16 @@
 
 import Skull from 'backbone-skull';
 
-function collectionIterator (models) {
-    return  {
-        _i: -1,
-        next: function () {
-            return {
-                value: models[this._i + 1].attributes,
-                done: models.length - 1 === ++this._i
-            }
-        }
+function* collectionIterator (models) {
+    for (let model of models) {
+        yield model.attributes;
     }
 }
 
 class CollectionAbstract extends Skull.Collection {
-    initialize () {
-        this[Symbol.iterator] = () => {
-            return collectionIterator(this.models)
-        }
+    [Symbol.iterator] () {
+        return collectionIterator(this.models);
     }
-
-    //[Symbol.iterator] () { babel doesn't like it:(
-    //  return collectionIterator(this.models)
-    //}
 }
 
 export default CollectionAbstract;
